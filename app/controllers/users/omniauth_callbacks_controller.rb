3f183a6d50fae4c3c8231f115b6566d64fe3264a
Sinister Light <me@sinisterlight.com>
2015-05-05 20:39:09 +0530

    [Shobhit] adding twitter account

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    @user = User.from_twitter(request.env["omniauth.auth"])
+    if current_user.nil?
+      @user = User.from_twitter(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication
+        set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      else
+        session["devise.twitter_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      auth = request.env["omniauth.auth"]
+      TwitterAccount.create uid: auth.uid, handle: auth.info.nickname, token: auth.credentials.token, secret: auth.credentials.secret, owner: current_user
+      redirect_to user_root_url
     end
   end
 end
--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,2 +1,15 @@
 class TwitterAccount < SocialAccount
+  field :uid
+  field :handle
+  field :token
+  field :secret
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter["consumer_key"]
+      config.consumer_secret     = Rails.application.secrets.twitter["consumer_secret"]
+      config.access_token        = self.token
+      config.access_token_secret = self.secret
+    end
+  end
 end
