ccf84efc9f7ca99545efdd960e1118d016418d86
Sinister Light <me@sinisterlight.com>
2015-05-05 14:24:09 +0530

    [Shobhit] forcing login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter["app_key"], Rails.application.secrets.twitter["app_secret"]
+  provider :twitter,
+    Rails.application.secrets.twitter["consumer_key"],
+    Rails.application.secrets.twitter["consumer_secret"],
+    { authorize_params: { force_login: 'true' } }
 end
