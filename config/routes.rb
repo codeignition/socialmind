2e9f2172bdb294867b32a73026f2cb9eb2e08489
Sinister Light <me@sinisterlight.com>
2015-05-05 21:10:39 +0530

    [Shobhit] cleaning social account routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :social_accounts
-  resources :organizations
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    authenticated :user do
+      resources :organizations
+      resources :social_accounts, only: [:index,:show,:destroy]
+      get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
+
+    unauthenticated :user do
+      get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    end
   end
 
   authenticated :user do
